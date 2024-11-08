352c443dd6704037c6dd7fae6cc1918cecf4b314
ablancas707 <ablancas@trinity.edu>
2024-11-07 21:28:54 -0600

    Initial Commit

---
+++ Muse/MuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseApp.swift
+//  Muse
+//
+//  Created by Aidan Blancas on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
